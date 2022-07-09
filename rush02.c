c3f41099c285fe753393330d344be3f4548fe7ed
ortegaa08 <108222031+ortegaa08@users.noreply.github.com>
2022-07-10 00:37:23 +0200

    Add files via upload

--- rush02.c
+++ rush02.c
@@ -10,24 +10,24 @@ void ft_rush02(int x, int y)
 	int f;
 	int c;
 
-	c = 1;
-	while (c <= y && x > 0)
+	f = 1; // inicializamos la variable fuera ya que no queremos repetir el proceso
+	while (f <= y && x > 0) // al ser f = 1 y tiene que ser mayor para que sea positivo y x tiene que ser mayor que 0 para que sea positivo y poner algo 
 	{
-		f = 1;
-		while(f <= x)
+		c = 1; // para que se reinicie cada vez que se suma una fila
+		while(c <= x) // para que cuando sea menor haga un bucle para las columnas 
 		{
-			if ((f == 1 && c == 1) || (f == x && c == 1))
+			if ((c == 1 && f == 1) || (c == x && f == 1)) //c == 1 es si esta en la primera columna y f == 1 la columna 1 // c == x para el extremo 
 				ft_putchar('A');
-			else if((f == 1 && c == y) || (f == x && c == y))
+			else if((c == 1 && f == y) || (c == x && f == y)) // c == 1 columna 1 y f == y es la ultima columna // c == x para el otro extremo de la ultima columna
 				ft_putchar('C');
-			else if((f == 1) || (f == x) || (c == 1) || (c == y))
+			else if((c == 1) || (c == x) || (f == 1) || (f == y)) // para que entre en cualquiera y no ocupe toda la zona, al tener un else if solo entra si no se cumplen
 				ft_putchar('B');
 			else
-				ft_putchar(' ');
-			f++;
+				ft_putchar(' '); //pone los espacios de entre medias//
+			c++;
 		}
-		ft_putchar('\n');
-		c++;
+		ft_putchar('\n'); //pasa siguiente fila
+		f++;  //suma una fila para que repita una fila hasta terminar todas las x y pasar a la siguiente
 	}
 }
 
